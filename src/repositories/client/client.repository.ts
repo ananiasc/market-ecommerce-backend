1102c03cb13b702a99e8b7c79c8cbe603e88dec1
Ananias Caetano <ananiascaetano556@gmail.com>
2025-04-27 16:39:21 -0300

    refactor: separating service from prisma and creating repository

---
+++ src/repositories/client/client.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { RegisterClient } from 'src/auth/entity/register-client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ClientRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(registerClient: RegisterClient, hashed: string) {
+    return await this.prisma.clients.create({
+      data: {
+        ...registerClient,
+        password: hashed,
+        is_active: true,
+      },
+    });
+  }
+
+  async findByEmail(email: string) {
+    return await this.prisma.clients.findUnique({ where: { email } });
+  }
+}
